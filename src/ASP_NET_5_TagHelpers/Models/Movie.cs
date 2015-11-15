10876a10763d6e6d95f74d6a0eb63540fd743c6f
Mustafa Erhan Ersoy <merhanersoy@gmail.com>
2015-11-15 22:23:14 +0200

    View Components örnekleri eklendi
    View components örnekleri eklendi.

---
+++ src/ASP_NET_5_TagHelpers/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_5_TagHelpers.Models
+{
+    public class Movie
+    {
+        public string ImdbId { set; get; }
+        public string Name { set; get; }
+    }
+}
---
+++ src/ASP_NET_5_TagHelpers/MoviesViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ASP_NET_5_TagHelpers.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace ASP_NET_5_TagHelpers
+{
+    public class MoviesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string imdbId)
+        {
+            List<Movie> movies = GetMovies();
+            if (string.IsNullOrEmpty(imdbId))
+            {
+                return View(GetMovies());
+            }
+            else
+            {
+                return View(GetMovies().Where(m => m.ImdbId == imdbId));
+            }
+        }
+
+        public List<Movie> GetMovies()
+        {
+            List<Movie> movieList = new List<Movie>();
+            movieList.Add(new Movie() { ImdbId = "tt0470752", Name = "Ex Machina" });
+            movieList.Add(new Movie() { ImdbId = "tt2562232", Name = "Birdman" });
+            movieList.Add(new Movie() { ImdbId = "tt0137523", Name = "Fight Club" });
+            return movieList;
+        }
+    }
+}
